60f8c6c94a53b209f1f03576e23ef1f87af81198
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2021-11-21 09:19:12 -0600

    Lint Update: Prettier rules apply to CSS files

--- bulls-and-cows/src/Greeting.jsx
+++ bulls-and-cows/src/Greeting.jsx
@@ -12,11 +12,19 @@ const Greeting = () => {
       <h1 className={styles.Title}>Bulls and Cows</h1>
       <div className={styles.Instruction}>
         <div>
-          <span className={`${styles.InstructionText} ${styles.AvoidWrap}`}>Discover four hidden numbers!&nbsp;</span>
-          <span className={`${styles.InstructionText} ${styles.AvoidWrap}`}>You have 10 chances to try.</span>
+          <span className={`${styles.InstructionText} ${styles.AvoidWrap}`}>
+            Discover four hidden numbers!&nbsp;
+          </span>
+          <span className={`${styles.InstructionText} ${styles.AvoidWrap}`}>
+            You have 10 chances to try.
+          </span>
         </div>
-        <span className={styles.InstructionText}>Bulls = Correct Number, Correct Position.</span>
-        <span className={styles.InstructionText}>Cows = Correct Number, Wrong Position.</span>
+        <span className={styles.InstructionText}>
+          Bulls = Correct Number, Correct Position.
+        </span>
+        <span className={styles.InstructionText}>
+          Cows = Correct Number, Wrong Position.
+        </span>
       </div>
     </div>
   );
