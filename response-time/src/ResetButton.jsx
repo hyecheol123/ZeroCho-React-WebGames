2635032917b4fe62a8f74a4e6e51a5fd129ac9d0
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2021-11-24 00:52:07 -0600

    response-time: ResetButton Design

--- response-time/src/ResetButton.jsx
+++ response-time/src/ResetButton.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from '../styles/ResetButton.module.css';
 
 /**
  * ResetButton React Component that shows button to reset the game.
@@ -8,7 +9,7 @@ import React from 'react';
  * @return {React.ReactElement} a react element referring ResetButton
  */
 const ResetButton = ({ resetFunc }) => {
-  return <button onClick={resetFunc}>Reset</button>;
+  return <button onClick={resetFunc} className={styles.Button}>Reset</button>;
 };
 
 export default React.memo(ResetButton);
