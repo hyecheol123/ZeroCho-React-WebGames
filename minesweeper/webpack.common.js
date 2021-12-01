25e147df1feef2e9af7f44d86e52fab0655f41fd
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2021-12-01 22:49:44 +0900

    minesweeper: Setup Project

---
+++ minesweeper/webpack.common.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './minesweeper/src/index.js',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: path.join(__dirname, '..', 'node_modules'),
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|jpeg|svg)$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 4 * 1024,
+          },
+        },
+        generator: {
+          filename: 'image/[hash][ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({ React: 'react' }),
+    new HtmlWebpackPlugin({
+      template: './minesweeper/public/index.html',
+    }),
+  ],
+};
